9f7666730b5a0d824b5622d615c24b8dbac22c52
Koki Matsuzawa <shingaryu@gmail.com>
2019-09-15 23:47:20 +0200

    Add a function to calculate the history of balance(specific currency) by time

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const app = express();
+const analysis = require('./analysis');
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+asyncMain();
 
-app.post('/', function(req, res) {
-  console.log(req.body);
-  res.send(`hello`);
-});
+async function asyncMain() {
+// const express = require('express');
+// const app = express();
 
-app.listen(8000, function() {
-  console.log('Example app listening on port 8000!');
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
 
+// app.post('/', function(req, res) {
+//   console.log(req.body);
+//   res.send(`hello`);
+// });
+
+// app.listen(8000, function() {
+//   console.log('Example app listening on port 8000!');
+// });
+
+  const timeSeries = await analysis.timeSeriesBalance('YEN', 0);
+  console.log(timeSeries);
+}
