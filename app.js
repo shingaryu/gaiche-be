0f8676cefeb9263af38134e10574291f6228bbde
Koki Matsuzawa <shingaryu@gmail.com>
2019-09-29 05:01:28 +0200

    Add CORS Setting

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const records = require('./controllers/recordController');
 
 app.get('/', function(req, res) {
